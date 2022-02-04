2b262b75e656903999a6db7aa160eeb5ab00987b
eliascoelho911 <elias_coelho911@hotmail.com>
2022-02-04 18:04:16 -0300

    refactor: Corrige nome da activity

--- app/src/main/java/com/github/eliascoelho911/robok/ui/RootActivity.kt
+++ app/src/main/java/com/github/eliascoelho911/robok/ui/RootActivity.kt
@@ -3,9 +3,9 @@ package com.github.eliascoelho911.robok.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.eliascoelho911.robok.R
-import kotlinx.android.synthetic.main.home_activity.*
+import kotlinx.android.synthetic.main.root_activity.*
 
-class RootActivity : AppCompatActivity(R.layout.home_activity) {
+class RootActivity : AppCompatActivity(R.layout.root_activity) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
