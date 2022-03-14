749dcf01a89cdb0fbd6d14722a40e5f573328776
eliascoelho911 <elias_coelho911@hotmail.com>
2022-03-14 14:53:44 -0300

    feat: implementa ModelCreator.kt

--- app/src/main/java/com/github/eliascoelho911/robok/ui/widgets/ReviewScannedCubeView.kt
+++ app/src/main/java/com/github/eliascoelho911/robok/ui/widgets/ReviewScannedCubeView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.widget.FrameLayout
 import androidx.annotation.AttrRes
 import com.github.eliascoelho911.robok.R
+import com.github.eliascoelho911.robok.rubikcube.Model
 import com.github.eliascoelho911.robok.rubikcube.RubikCube
 import kotlinx.android.synthetic.main.review_scanned_cube.view.preview_cube_view
 
@@ -17,17 +18,11 @@ class ReviewScannedCubeView @JvmOverloads constructor(
         inflate(context, R.layout.review_scanned_cube, this)
     }
 
-    //todo depender de uma interface
     fun show(rubikCube: RubikCube) {
-//        val up = "402415221"
-//        val down = "542204233"
-//        val front = "311244100"
-//        val back = "310551454"
-//        val left = "031221300"
-//        val right = "555330435"
-//        val expected = "$up$down$front$back$left$right"
-            previewCubeView.setCubeModel(rubikCube.model)
-            previewCubeView.setCubeColors(rubikCube.distinctColors.toIntArray())
+        previewCubeView.apply {
+            setCubeModel(rubikCube.model)
+            setCubeColors(rubikCube.distinctColors.toIntArray())
+        }
     }
 
     private val previewCubeView by lazy { preview_cube_view }
