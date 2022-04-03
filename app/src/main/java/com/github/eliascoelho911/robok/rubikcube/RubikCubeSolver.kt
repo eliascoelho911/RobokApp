1dabe02db909f6b85c687c41b1f2d3df36fa764a
eliascoelho911 <elias_coelho911@hotmail.com>
2022-04-03 13:32:29 -0300

    feat: Mostra os movimentos na resolução

--- app/src/main/java/com/github/eliascoelho911/robok/rubikcube/RubikCubeSolver.kt
+++ app/src/main/java/com/github/eliascoelho911/robok/rubikcube/RubikCubeSolver.kt
@@ -7,9 +7,12 @@ import kotlinx.coroutines.withContext
 class RubikCubeSolver(private val modelCreator: Min2PhaseModelCreator) {
     suspend fun solve(rubikCube: RubikCube) = findShorterSolutions(rubikCube)
 
-    private suspend fun findShorterSolutions(rubikCube: RubikCube): String =
+    private suspend fun findShorterSolutions(rubikCube: RubikCube): List<Move> =
         withContext(Dispatchers.Default) {
             val model = rubikCube.createModelWith(modelCreator)
             Search().solution(model, 21, 100000000, 10000, 0)
+                .replace("  ", " ").split(" ").filterNot { it.isBlank() }
         }
 }
+
+typealias Move = String
--- app/src/main/java/com/github/eliascoelho911/robok/ui/screens/RubikCubeSolveFragment.kt
+++ app/src/main/java/com/github/eliascoelho911/robok/ui/screens/RubikCubeSolveFragment.kt
@@ -1,15 +1,20 @@
 package com.github.eliascoelho911.robok.ui.screens
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.github.eliascoelho911.robok.R
 import com.github.eliascoelho911.robok.rubikcube.AnimCubeModelCreator
+import com.github.eliascoelho911.robok.rubikcube.Move
 import com.github.eliascoelho911.robok.rubikcube.RubikCubeSolver
 import com.github.eliascoelho911.robok.ui.dialogs.LoadingDialog
 import com.github.eliascoelho911.robok.ui.screens.RubikCubeSolveFragmentDirections.Companion.actionRubikCubeSolveFragmentToCaptureFragment
@@ -17,6 +22,7 @@ import com.github.eliascoelho911.robok.ui.viewmodels.RubikCubeSolveViewModel
 import kotlinx.android.synthetic.main.rubik_cube_solve_fragment.fab_confirm
 import kotlinx.android.synthetic.main.rubik_cube_solve_fragment.fab_retry
 import kotlinx.android.synthetic.main.rubik_cube_solve_fragment.preview_cube_view
+import kotlinx.android.synthetic.main.rubik_cube_solve_fragment.txt_moves
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -39,6 +45,8 @@ class RubikCubeSolveFragment : Fragment() {
     private val previewCubeView by lazy { preview_cube_view }
     private val confirmFabView by lazy { fab_confirm }
     private val retryFabView by lazy { fab_retry }
+    private val movesTextView by lazy { txt_moves }
+    private var currentMoveIndex = -1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,7 +63,7 @@ class RubikCubeSolveFragment : Fragment() {
 
     private fun setupRuleForConnectionToRobot() {
         viewModel.isConnectedWithRobot.observe(viewLifecycleOwner) { isConnected ->
-            confirmFabView.isEnabled = isConnected
+//            confirmFabView.isClickable = isConnected
         }
     }
 
@@ -74,12 +82,47 @@ class RubikCubeSolveFragment : Fragment() {
     private fun solveRubikCube() {
         solvingDialog.show()
         lifecycleScope.launch {
-            rubikCubeSolver.solve(rubikCube).let {
-                previewCubeView.setMoveSequence(it)
+            rubikCubeSolver.solve(rubikCube).let { moves ->
+                val moveSequence = moves.joinToString(separator = " ")
+                previewCubeView.setMoveSequence(moveSequence)
+
+                previewCubeView.setOnAnimationFinishedListener {
+                    startNextMove(moveSequence, moves)
+                }
+                startNextMove(moveSequence, moves)
             }
             solvingDialog.dismiss()
-            previewCubeView.animateMoveSequence()
         }
+    }
+
+    private fun startNextMove(
+        moveSequence: String,
+        moves: List<Move>,
+    ) {
+        val nextMoveIndex = currentMoveIndex + 1
+        if (nextMoveIndex <= moves.lastIndex) {
+            currentMoveIndex++
+            movesTextView.text = spannableWithCurrentMoveInHighlight(moveSequence, moves)
+            previewCubeView.animateMove()
+        }
+    }
+
+    private fun spannableWithCurrentMoveInHighlight(
+        moveSequence: String,
+        moves: List<Move>,
+    ): SpannableString {
+        val movesSpannable = SpannableString(moveSequence)
+        val color = ContextCompat.getColor(requireContext(), R.color.green_a400)
+        val nextMove = moves[currentMoveIndex]
+        val previousMovesLength = moves.subList(0, currentMoveIndex).sumOf { it.length }
+        val spacesLength = currentMoveIndex
+        val start = spacesLength + previousMovesLength
+        val end = start + nextMove.length
+        movesSpannable.setSpan(ForegroundColorSpan(color),
+            start,
+            end,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        return movesSpannable
     }
 
     private fun hideButtons() {
